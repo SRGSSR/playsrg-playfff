7537b53520d1c3bf4ad37adc8babd547422caccd
sebastien <sebastien.chauvin@gmail.com>
2018-03-20 11:18:54 +0100

    Do not output null fields

--- src/main/java/com/example/pfff/model/UpdateResult.java
+++ src/main/java/com/example/pfff/model/UpdateResult.java
@@ -1,10 +1,14 @@
 package com.example.pfff.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.sun.istack.internal.Nullable;
+
 /**
  * Copyright (c) SRG SSR. All rights reserved.
  * <p>
  * License information is available from the LICENSE file.
  */
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UpdateResult {
     /**
      * Text to be displayed, may contain unicode characters.
--- src/main/java/com/example/pfff/model/WhatIsNewResult.java
+++ src/main/java/com/example/pfff/model/WhatIsNewResult.java
@@ -1,22 +1,20 @@
 package com.example.pfff.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 /**
  * Copyright (c) SRG SSR. All rights reserved.
  * <p>
  * License information is available from the LICENSE file.
  */
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class WhatIsNewResult {
     /**
      * Text to be displayed, may contain unicode characters.
      */
     public String text;
-    /**
-     * Title. If null, client default string is displayed.
-     */
-    public String title;
 
     public WhatIsNewResult(String text) {
         this.text = text;
     }
-
 }
