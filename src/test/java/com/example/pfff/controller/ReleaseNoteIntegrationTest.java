af843cd9bb565475a067ca19bcd030b695da49a3
sebastien <sebastien.chauvin@gmail.com>
2018-03-20 18:29:34 +0100

    Use mock mvc for integration tests. Fix tests

--- src/test/java/com/example/pfff/controller/ReleaseNoteIntegrationTest.java
+++ src/test/java/com/example/pfff/controller/ReleaseNoteIntegrationTest.java
@@ -1,32 +1,53 @@
 package com.example.pfff.controller;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ReleaseNoteIntegrationTest {
-    @LocalServerPort
-    private int port;
+    private MockMvc mvc;
 
     @Autowired
-    private TestRestTemplate restTemplate;
+    private WebApplicationContext context;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
 
     @Test
+    @WithMockUser(username = "user", password = "password", roles = "USER")
     public void changeReleaseNoteTwice() throws Exception {
-        assertThat(this.restTemplate.postForObject("http://localhost:" + port + "/whatisnew/save?package=package.A.B&version=0.1.2&text=", null, String.class)).contains("pushed");
+        mvc.perform(get("/whatisnew/admin")).andExpect(status().isOk());
 
-        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/whatisnew/text?package=package.A.B&version=0.1.2", String.class)).contains("{\"text\":\"\"}");
+        mvc.perform(post("/whatisnew/admin").param("package", "package.A.B").param("version", "0.1.2").param("text", "").with(csrf())).andExpect(status().isOk()).andExpect(content().string("pushed"));
 
-        assertThat(this.restTemplate.postForObject("http://localhost:" + port + "/whatisnew/save?package=package.A.B&version=0.1.2&text=test012OK", null, String.class)).contains("pushed");
+        mvc.perform(get("/whatisnew/text").param("package", "package.A.B").param("version", "0.1.2")).andExpect(status().isOk()).andExpect(content().json("{\"text\":\"\"}"));
 
-        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/whatisnew/text?package=package.A.B&version=0.1.2", String.class)).contains("{\"text\":\"test012OK\"}");
+        mvc.perform(post("/whatisnew/admin").param("package", "package.A.B").param("version", "0.1.2").param("text", "test012OK").with(csrf())).andExpect(status().isOk()).andExpect(content().string("pushed"));
+
+        mvc.perform(get("/whatisnew/text").param("package", "package.A.B").param("version", "0.1.2")).andExpect(status().isOk()).andExpect(content().json("{\"text\":\"test012OK\"}"));
     }
 }
