8fd0055b99e9e8266ad91f1db82028c52ac1163b
sebastien <sebastien.chauvin@gmail.com>
2018-06-25 11:46:42 +0200

    Recommendation end point using current API

---
+++ src/main/java/com/example/pfff/controller/RecommendationController.java
@@ -0,0 +1,34 @@
+package com.example.pfff.controller;
+
+import com.example.pfff.repository.RecommendationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
+
+/**
+ * Copyright (c) SRG SSR. All rights reserved.
+ * <p>
+ * License information is available from the LICENSE file.
+ */
+@Controller
+public class RecommendationController {
+    @Autowired
+    RecommendationService service;
+
+    @RequestMapping("/api/v1/recommendation/{purpose}/{urn}")
+    @ResponseBody
+    ModelAndView recommendation(
+            @PathVariable("purpose") String purpose,
+            @PathVariable("urn") String urn,
+            @RequestParam("standAlone") Boolean standAlone) {
+        List<String> urns = service.getRecommendedUrns(purpose, urn, standAlone);
+        return new ModelAndView(new RedirectView("http://il.srgssr.ch/integrationlayer/2.0/mediaList/byUrns.json?urns=" + String.join(",", urns)));
+    }
+}
---
+++ src/main/java/com/example/pfff/repository/RecommendationService.java
@@ -0,0 +1,25 @@
+package com.example.pfff.repository;
+
+import com.example.pfff.model.peach.RecommendationResult;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Repository
+public class RecommendationService {
+
+    private RestTemplate restTemplate;
+
+    public RecommendationService() {
+        restTemplate = new RestTemplate();
+    }
+
+    public List<String> getRecommendedUrns(String purpose, String urn, Boolean standAlone) {
+        String url = "https://peach.ebu.io/api/v1/chrts/continuous_playback_mobile?urn=" + urn + "&pageSize=50";
+
+        return restTemplate.exchange(url, HttpMethod.GET, null, RecommendationResult.class).getBody().getUrns();
+    }
+}
