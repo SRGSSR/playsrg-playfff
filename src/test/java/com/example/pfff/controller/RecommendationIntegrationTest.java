3ce5c742ab251fe200f063359499185083a1e8e6
sebastien <sebastien.chauvin@gmail.com>
2018-07-09 11:18:24 +0200

    Integration test checks IL result validity

--- src/test/java/com/example/pfff/controller/RecommendationIntegrationTest.java
+++ src/test/java/com/example/pfff/controller/RecommendationIntegrationTest.java
@@ -1,14 +1,21 @@
 package com.example.pfff.controller;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -37,9 +44,23 @@ public class RecommendationIntegrationTest {
 
         mvc.perform(get("/api/v1/playlist/recommendation/" + purpose + "/" + mediaURN)).andExpect(status().isFound()).andExpect(content().string(""));
 
-        mvc.perform(get("/api/v1/playlist/recommendation/" + purpose + "/" + mediaURN).param("standalone", "false")).andExpect(status().isFound()).andExpect(content().string(""));
+        checkRecommendationWithRedirect(purpose, mediaURN, "false");
 
-        mvc.perform(get("/api/v1/playlist/recommendation/" + purpose + "/" + mediaURN).param("standalone", "true")).andExpect(status().isFound()).andExpect(content().string(""));
+        checkRecommendationWithRedirect(purpose, mediaURN, "true");
+    }
+
+    private void checkRecommendationWithRedirect(String purpose, String mediaURN, String standalone) throws Exception {
+        MvcResult mvcResult = mvc.perform(get("/api/v1/playlist/recommendation/" + purpose + "/" + mediaURN).param("standalone", standalone)).andExpect(status().is3xxRedirection()).andExpect(content().string("")).andReturn();
+        String location = mvcResult.getResponse().getHeader("Location");
+        System.out.println(location);
+        RestTemplate restTemplate = new RestTemplate();
+        MediaListResult result = restTemplate.exchange(location, HttpMethod.GET, null, MediaListResult.class).getBody();
+        Assert.assertNotNull(result.mediaList);
+    }
+
+    static class MediaListResult {
+        @JsonProperty("mediaList")
+        List<Object> mediaList;
     }
 
     @Test
