ad896aec9c5db54a53b1a721b6fb262c4b2d14da
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2018-07-15 14:08:46 +0200

    Version api returns version of the application

--- src/main/java/com/example/pfff/controller/VersionController.java
+++ src/main/java/com/example/pfff/controller/VersionController.java
@@ -1,5 +1,9 @@
 package com.example.pfff.controller;
 
+import org.assertj.core.util.VisibleForTesting;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,12 +14,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
  * License information is available from the LICENSE file.
  */
 @Controller
-public class Version {
-    public static final String VERSION = "Pfff...";
+public class VersionController {
+
+    @Value("${version}")
+    private String version;
 
     @RequestMapping("/api/v1/version")
     @ResponseBody
-    String home() {
-        return VERSION;
+    public ResponseEntity<String> version() {
+        return new ResponseEntity<>(version, HttpStatus.OK);
+    }
+
+    @VisibleForTesting
+    public String getVersion() {
+        return version;
     }
 }
