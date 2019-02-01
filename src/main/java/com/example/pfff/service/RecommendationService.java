e2856194c9c27446decc11bac5ed9626c0df286d
amtins <andre.martins@rts.ch>
2019-02-01 17:57:10 +0100

    Extract rts audio recommended list

--- src/main/java/com/example/pfff/service/RecommendationService.java
+++ src/main/java/com/example/pfff/service/RecommendationService.java
@@ -38,72 +38,76 @@ public class RecommendationService {
             if (urn.contains(":video:")) {
                 return rtsVideoRecommendedList(purpose, urn, standalone);
             } else if (urn.contains(":audio:")) {
-                Media media = integrationLayerRequest.getMedia(urn, Environment.PROD);
-                if (media == null || media.getType() == LIVESTREAM || media.getType() == SCHEDULED_LIVESTREAM || media.getShow() == null) {
-                    return new RecommendedList();
-                }
-
-                EpisodeComposition episodeComposition = integrationLayerRequest.getEpisodeCompositionLatestByShow(media.getShow().getUrn(), null, 100, Environment.PROD);
-                if (episodeComposition == null) {
-                    return new RecommendedList();
-                }
-
-                List<EpisodeWithMedias> episodes = Lists.reverse(episodeComposition.getList());
-                List<String> fullLengthUrns = episodes.stream().map(EpisodeWithMedias::getFullLengthUrn).collect(Collectors.toList());
-                List<String> clipUrns = episodes.stream().flatMap(e -> e.getMediaList().stream().filter(m -> m.getMediaType() == MediaType.AUDIO)).map(Media::getUrn).collect(Collectors.toList());
-                clipUrns.removeAll(fullLengthUrns);
-
-                Boolean isFullLengthUrns = false;
-                List<String> recommendationResult = null;
-
-                List<String> urns = null;
-                int index = -1;
-
-                if (fullLengthUrns.contains(urn)) {
-                    isFullLengthUrns = true;
-                    index = fullLengthUrns.lastIndexOf(urn);
-                    urns = fullLengthUrns;
-                } else if (clipUrns.contains(urn)) {
-                    isFullLengthUrns = false;
-                    index = clipUrns.lastIndexOf(urn);
-                    urns = clipUrns;
-                } else {
-                    isFullLengthUrns = media.getType() != CLIP;
-                    urns = isFullLengthUrns ? fullLengthUrns : clipUrns;
-                }
-
-                // First: newest medias in date ascending order. Then:
-                // - if `nextUrl` exists (show has more than 100 episodes), oldest medias in date descending order.
-                // - else (show has less than 100 episodes), oldest medias in date ascending order.
-                if (index > -1 && index < urns.size() - 1) {
-                    recommendationResult = new ArrayList<>(urns.subList(index + 1, urns.size()));
-                    urns.removeAll(recommendationResult);
-                    urns.remove(urn);
-                } else {
-                    // Latest urn or not found urn
-                    recommendationResult = new ArrayList<>();
-                }
-
-                if (episodeComposition.getNext() != null) {
-                    recommendationResult.addAll(Lists.reverse(urns));
-                } else {
-                    recommendationResult.addAll(urns);
-                }
-
-                String host = "playfff.srgssr.ch";
-                String recommendationId = "EpisodeComposition/LatestByShow/" + (isFullLengthUrns ? "FullLength" : "Clip");
-
-                if (recommendationResult.size() > 49) {
-                    recommendationResult = recommendationResult.subList(0, 49);
-                }
-
-                return new RecommendedList(host, recommendationId, recommendationResult);
+                return rtsAudioRecommendedList(urn);
             } else {
                 return new RecommendedList();
             }
         } else {
             return new RecommendedList();
         }
+    }
+
+    private RecommendedList rtsAudioRecommendedList(String urn) {
+        Media media = integrationLayerRequest.getMedia(urn, Environment.PROD);
+        if (media == null || media.getType() == LIVESTREAM || media.getType() == SCHEDULED_LIVESTREAM || media.getShow() == null) {
+            return new RecommendedList();
+        }
+
+        EpisodeComposition episodeComposition = integrationLayerRequest.getEpisodeCompositionLatestByShow(media.getShow().getUrn(), null, 100, Environment.PROD);
+        if (episodeComposition == null) {
+            return new RecommendedList();
+        }
+
+        List<EpisodeWithMedias> episodes = Lists.reverse(episodeComposition.getList());
+        List<String> fullLengthUrns = episodes.stream().map(EpisodeWithMedias::getFullLengthUrn).collect(Collectors.toList());
+        List<String> clipUrns = episodes.stream().flatMap(e -> e.getMediaList().stream().filter(m -> m.getMediaType() == MediaType.AUDIO)).map(Media::getUrn).collect(Collectors.toList());
+        clipUrns.removeAll(fullLengthUrns);
+
+        Boolean isFullLengthUrns = false;
+        List<String> recommendationResult = null;
+
+        List<String> urns = null;
+        int index = -1;
+
+        if (fullLengthUrns.contains(urn)) {
+            isFullLengthUrns = true;
+            index = fullLengthUrns.lastIndexOf(urn);
+            urns = fullLengthUrns;
+        } else if (clipUrns.contains(urn)) {
+            isFullLengthUrns = false;
+            index = clipUrns.lastIndexOf(urn);
+            urns = clipUrns;
+        } else {
+            isFullLengthUrns = media.getType() != CLIP;
+            urns = isFullLengthUrns ? fullLengthUrns : clipUrns;
+        }
+
+        // First: newest medias in date ascending order. Then:
+        // - if `nextUrl` exists (show has more than 100 episodes), oldest medias in date descending order.
+        // - else (show has less than 100 episodes), oldest medias in date ascending order.
+        if (index > -1 && index < urns.size() - 1) {
+            recommendationResult = new ArrayList<>(urns.subList(index + 1, urns.size()));
+            urns.removeAll(recommendationResult);
+            urns.remove(urn);
+        } else {
+            // Latest urn or not found urn
+            recommendationResult = new ArrayList<>();
+        }
+
+        if (episodeComposition.getNext() != null) {
+            recommendationResult.addAll(Lists.reverse(urns));
+        } else {
+            recommendationResult.addAll(urns);
+        }
+
+        String host = "playfff.srgssr.ch";
+        String recommendationId = "EpisodeComposition/LatestByShow/" + (isFullLengthUrns ? "FullLength" : "Clip");
+
+        if (recommendationResult.size() > 49) {
+            recommendationResult = recommendationResult.subList(0, 49);
+        }
+
+        return new RecommendedList(host, recommendationId, recommendationResult);
     }
 
     private RecommendedList rtsVideoRecommendedList(String purpose, String urn, boolean standalone) {
