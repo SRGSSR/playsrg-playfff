0b0477bfce158d3d270389274d75141c1ae5c1c4
Sebastien Chauvin <sebastien.chauvin@rts.ch>
2019-02-12 08:57:40 +0100

    Simplify API

--- src/main/java/com/example/pfff/controller/RecommendationController.java
+++ src/main/java/com/example/pfff/controller/RecommendationController.java
@@ -4,10 +4,7 @@ import com.example.pfff.model.RecommendedList;
 import com.example.pfff.service.RecommendationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -63,12 +60,11 @@ public class RecommendationController {
     }
 
 
-    @RequestMapping("/api/v2/playlist/personal_recommendation/{type}/{userId}")
+    @RequestMapping("/api/v2/playlist/personalRecommendation/{userId}")
     @ResponseBody
     RecommendedList personalRecommendation(
-            HttpServletRequest request,
-            @PathVariable("type") String type, @PathVariable("userId") String userId) {
-        return service.getPersonalRecommendation(type, userId);
+            HttpServletRequest request, @PathVariable("userId") String userId) {
+        return service.getPersonalRecommendation("rtsPeachHome", userId);
     }
 
 }
