7a4a0f2ff6ad78e73eb7a0d17eec3c6097d613c8
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2019-05-05 17:42:33 +0200

    Implement ETag and If-None-Match

--- src/main/java/ch/srgssr/playfff/controller/DeeplinkController.java
+++ src/main/java/ch/srgssr/playfff/controller/DeeplinkController.java
@@ -3,8 +3,10 @@ package ch.srgssr.playfff.controller;
 import ch.srgssr.playfff.model.DeeplinkContent;
 import ch.srgssr.playfff.service.DeeplinkService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import java.net.URISyntaxException;
@@ -23,16 +25,19 @@ public class DeeplinkController {
 
     @RequestMapping("/v1/parse_play_url.js")
     @ResponseBody
-    public ResponseEntity<String> parsePlayUrlJavascript() throws URISyntaxException {
+    public ResponseEntity<String> parsePlayUrlJavascript(@RequestHeader(value = "If-None-Match", required = false) String ifNoneMatchHeader) throws URISyntaxException {
 
         DeeplinkContent deeplinkContent = service.getParsePlayUrlContent();
 
         if (deeplinkContent != null) {
-            return ResponseEntity.ok()
-                    .header("ETag", deeplinkContent.getHash())
-                    .body(deeplinkContent.getContent());
-        }
-        else {
+            if (ifNoneMatchHeader != null && ifNoneMatchHeader.equals(deeplinkContent.getHash())) {
+                return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
+            } else {
+                return ResponseEntity.ok()
+                        .header("ETag", deeplinkContent.getHash())
+                        .body(deeplinkContent.getContent());
+            }
+        } else {
             return ResponseEntity.notFound().build();
         }
     }
--- src/test/java/ch/srgssr/playfff/controller/DeeplinkIntegrationTest.java
+++ src/test/java/ch/srgssr/playfff/controller/DeeplinkIntegrationTest.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -33,9 +34,14 @@ public class DeeplinkIntegrationTest {
 
     @Test
     public void getParsePlayUrl() throws Exception {
-        mvc.perform(get("/deeplink/v1/parse_play_url.js"))
+        MvcResult mvcResult = mvc.perform(get("/deeplink/v1/parse_play_url.js"))
                 .andExpect(status().isOk())
                 .andExpect(header().string("ETag", IsNull.notNullValue()))
-                .andExpect(content().string(IsNull.notNullValue()));
+                .andExpect(content().string(IsNull.notNullValue())).andReturn();
+        String eTag = mvcResult.getResponse().getHeader("ETag");
+
+        mvc.perform(get("/deeplink/v1/parse_play_url.js").header("If-None-Match", eTag))
+                .andExpect(status().isNotModified())
+                .andExpect(content().string(""));
     }
 }
