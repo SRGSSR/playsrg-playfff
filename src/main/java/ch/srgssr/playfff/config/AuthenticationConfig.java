ed13d76c477b41713a0ef13b8a96bdfb57ac6d9c
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2019-05-11 15:38:22 +0200

    Fix cache headers for parse_play_url.js

--- src/main/java/ch/srgssr/playfff/config/AuthenticationConfig.java
+++ src/main/java/ch/srgssr/playfff/config/AuthenticationConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -49,6 +50,13 @@ public class AuthenticationConfig extends WebSecurityConfigurerAdapter {
                 .permitAll();
         http
                 .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/deeplink/v1/parse_play_url.js");
     }
 
     @Bean
--- src/main/java/ch/srgssr/playfff/controller/DeeplinkController.java
+++ src/main/java/ch/srgssr/playfff/controller/DeeplinkController.java
@@ -3,13 +3,11 @@ package ch.srgssr.playfff.controller;
 import ch.srgssr.playfff.model.DeeplinkContent;
 import ch.srgssr.playfff.service.DeeplinkService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.CacheControl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import java.net.URISyntaxException;
 
 /**
  * Copyright (c) SRG SSR. All rights reserved.
@@ -23,20 +21,17 @@ public class DeeplinkController {
     @Autowired
     private DeeplinkService service;
 
-    @RequestMapping("/v1/parse_play_url.js")
+    @RequestMapping(value="/v1/parse_play_url.js")
     @ResponseBody
-    public ResponseEntity<String> parsePlayUrlJavascript(@RequestHeader(value = "If-None-Match", required = false) String ifNoneMatchHeader) throws URISyntaxException {
+    public ResponseEntity<String> parsePlayUrlJavascript() {
 
         DeeplinkContent deeplinkContent = service.getParsePlayUrlContent();
 
         if (deeplinkContent != null) {
-            if (ifNoneMatchHeader != null && ifNoneMatchHeader.equals(deeplinkContent.getHash())) {
-                return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
-            } else {
-                return ResponseEntity.ok()
-                        .header("ETag", deeplinkContent.getHash())
-                        .body(deeplinkContent.getContent());
-            }
+            return ResponseEntity.ok()
+                    .cacheControl(CacheControl.empty().cachePublic())
+                    .eTag(deeplinkContent.getHash())
+                    .body(deeplinkContent.getContent());
         } else {
             return ResponseEntity.notFound().build();
         }
