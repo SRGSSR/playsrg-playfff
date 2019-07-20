546c840f5139483b7c46e64b536191d6bc89c4e5
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2019-07-20 16:47:15 +0200

    Add unit test for deeplink report post

--- src/test/java/ch/srgssr/playfff/controller/DeeplinkIntegrationTest.java
+++ src/test/java/ch/srgssr/playfff/controller/DeeplinkIntegrationTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -14,6 +15,7 @@ import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
@@ -43,5 +45,54 @@ public class DeeplinkIntegrationTest {
         mvc.perform(get("/api/v1/deeplink/parse_play_url.js").header("If-None-Match", eTag))
                 .andExpect(status().isNotModified())
                 .andExpect(content().string(""));
+    }
+
+    @Test
+    public void reportSave() throws Exception {
+        mvc.perform(post("/api/v1/deeplink/report")).andExpect(status().isBadRequest());
+
+        mvc.perform(post("/api/v1/deeplink/report").contentType(MediaType.APPLICATION_JSON).content("{}")).andExpect(status().isNotAcceptable());
+
+        String nonAcceptableJson1 = "{\n" +
+                "\t\"url\": \"https:\\/\\/www.rts.ch\\/rts\\/play\\/unknown\"\n" +
+                "}";
+
+        mvc.perform(post("/api/v1/deeplink/report").contentType(MediaType.APPLICATION_JSON).content(nonAcceptableJson1)).andExpect(status().isNotAcceptable());
+
+        String nonAcceptableJson2 = "{\n" +
+                "\t\"jsVersion\": \"v1.0\",\n" +
+                "\t\"url\": \"https:\\/\\/www.rts.ch\\/rts\\/play\\/unknown\"\n" +
+                "}";
+
+        mvc.perform(post("/api/v1/deeplink/report").contentType(MediaType.APPLICATION_JSON).content(nonAcceptableJson2)).andExpect(status().isNotAcceptable());
+
+        String nonAcceptableJson3 = "{\n" +
+                "\t\"clientId\": \"ch.rts.rtsplayer.debug\",\n" +
+                "\t\"jsVersion\": \"v1.0\",\n" +
+                "\t\"url\": \"https:\\/\\/www.rts.ch\\/rts\\/play\\/unknown\"\n" +
+                "}";
+
+        mvc.perform(post("/api/v1/deeplink/report").contentType(MediaType.APPLICATION_JSON).content(nonAcceptableJson3)).andExpect(status().isNotAcceptable());
+
+        String acceptableJson1 = "{\n" +
+                "\t\"clientTime\": \"2019-07-20T16:15:53+02:00\",\n" +
+                "\t\"clientId\": \"ch.rts.rtsplayer.debug\",\n" +
+                "\t\"jsVersion\": \"v1.0\",\n" +
+                "\t\"url\": \"https:\\/\\/www.rts.ch\\/rts\\/play\\/unknown\"\n" +
+                "}";
+
+        mvc.perform(post("/api/v1/deeplink/report").contentType(MediaType.APPLICATION_JSON).content(acceptableJson1)).andExpect(status().isCreated()).andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)).andExpect(jsonPath("$.id").value(1)).andExpect(jsonPath("$.count").value(1));
+
+        mvc.perform(post("/api/v1/deeplink/report").contentType(MediaType.APPLICATION_JSON).content(acceptableJson1)).andExpect(status().isCreated()).andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)).andExpect(jsonPath("$.id").value(1)).andExpect(jsonPath("$.count").value(2));
+
+        String acceptableJson2 = "{\n" +
+                "\t\"clientTime\": \"2019-07-20T16:15:53+02:00\",\n" +
+                "\t\"clientId\": \"ch.rts.rtsplayer.debug\",\n" +
+                "\t\"jsVersion\": \"v1.0\",\n" +
+                "\t\"url\": \"https:\\/\\/www.rts.ch\\/rts\\/play\\/unknown2\"\n" +
+                "}";
+
+        mvc.perform(post("/api/v1/deeplink/report").contentType(MediaType.APPLICATION_JSON).content(acceptableJson2)).andExpect(status().isCreated()).andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)).andExpect(jsonPath("$.id").value(2)).andExpect(jsonPath("$.count").value(1));
+
     }
 }
