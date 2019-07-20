6fab1cacf3da8cbb9a79c061e26f063a1c5f0218
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2019-07-20 12:17:46 +0200

    Resolve test execution

--- src/test/java/ch/srgssr/playfff/controller/RecommendationServiceAudioResultTests.java
+++ src/test/java/ch/srgssr/playfff/controller/RecommendationServiceAudioResultTests.java
@@ -23,7 +23,6 @@ import org.springframework.web.client.RestTemplate;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
@@ -206,7 +205,7 @@ public class RecommendationServiceAudioResultTests {
 
     private void testAudioRecommendation(String urn, boolean isFullLength, boolean episodeCompositionHasNextUrl, List<String> expectedUrns) throws URISyntaxException {
         String mediaFileName = urn.replace(":", "-") + ".json";
-        String mediaJson = BaseResourceString.getString(applicationContext, mediaFileName, new HashMap<>());
+        String mediaJson = BaseResourceString.getString(applicationContext, mediaFileName);
         mockServer.expect(ExpectedCount.times(2),
                 requestTo(new URI("http://il.srgssr.ch:80/integrationlayer/2.0/media/byUrn/"+ urn + ".json")))
                 .andExpect(method(HttpMethod.GET))
@@ -216,7 +215,7 @@ public class RecommendationServiceAudioResultTests {
                 );
 
         String episodeCompositionFileName = episodeCompositionHasNextUrl ? "episode-composition-rts-radio-next-url.json" : "episode-composition-rts-radio.json";
-        String episodeCompositionJson = BaseResourceString.getString(applicationContext, episodeCompositionFileName, new HashMap<>());
+        String episodeCompositionJson = BaseResourceString.getString(applicationContext, episodeCompositionFileName);
         mockServer.expect(ExpectedCount.times(2),
                 requestTo(new URI("http://il.srgssr.ch:80/integrationlayer/2.0/episodeComposition/latestByShow/byUrn/urn:rts:show:radio:1234.json?pageSize=100")))
                 .andExpect(method(HttpMethod.GET))
@@ -227,7 +226,7 @@ public class RecommendationServiceAudioResultTests {
 
         try {
             String mediaCompositionFileName = "media-composition-" + urn.replace(":", "-") + ".json";
-            String mediaCompositionJson = BaseResourceString.getString(applicationContext, mediaCompositionFileName, new HashMap<>());
+            String mediaCompositionJson = BaseResourceString.getString(applicationContext, mediaCompositionFileName);
             mockServer.expect(ExpectedCount.between(0, 2),
                     requestTo(new URI("http://il.srgssr.ch:80/integrationlayer/2.0/mediaComposition/byUrn/" + urn + ".json")))
                     .andExpect(method(HttpMethod.GET))
