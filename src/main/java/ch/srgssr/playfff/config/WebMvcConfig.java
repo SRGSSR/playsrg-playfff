3eb12ccb131490b19a050761b8d8796f0900807d
Josep Boix Requesens <jspboix@gmail.com>
2023-11-01 17:57:17 +0100

    Test serving the angular app from storage

--- src/main/java/ch/srgssr/playfff/config/WebMvcConfig.java
+++ src/main/java/ch/srgssr/playfff/config/WebMvcConfig.java
@@ -1,8 +1,15 @@
 package ch.srgssr.playfff.config;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 /**
  * Copyright (c) SRG SSR. All rights reserved.
@@ -12,11 +19,34 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurerAdapter {
 
-    @Override
-    public void addViewControllers(final ViewControllerRegistry registry) {
-        super.addViewControllers(registry);
-        registry.addViewController("/updates").setViewName("forward:/");
-        registry.addViewController("/add_update").setViewName("forward:/");
-        registry.addViewController("/deeplink").setViewName("forward:/");
-    }
+  private final ApplicationContext applicationContext;
+
+  @Autowired
+  public WebMvcConfig(ApplicationContext applicationContext) {
+    this.applicationContext = applicationContext;
+  }
+
+  @Override
+  public void addViewControllers(final ViewControllerRegistry registry) {
+    super.addViewControllers(registry);
+    registry.addViewController("/updates").setViewName("forward:/");
+    registry.addViewController("/add_update").setViewName("forward:/");
+    registry.addViewController("/deeplink").setViewName("forward:/");
+  }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/**")
+        .addResourceLocations("/portal-app/dist/admin-app")
+        .setCachePeriod(3600)
+        .resourceChain(true)
+        .addResolver(new PathResourceResolver() {
+          @Override
+          protected Resource getResource(String resourcePath, Resource location) throws IOException {
+            Resource requestedResource = location.createRelative(resourcePath);
+            return requestedResource.exists() && requestedResource.isReadable() ? requestedResource
+                : applicationContext.getResource("/portal-app/dist/admin-app/index.html");
+          }
+        });
+  }
 }
