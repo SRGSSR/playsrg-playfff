0f964a3c42b6f1281fa6deaaa4f7cb5b7b45137e
Pierre-Yves Bertholon <py.bertholon@gmail.com>
2023-11-05 23:31:58 +0100

    Revert "Don't need a bean"
    This reverts commit fe5baa8fe0e402ed8f4ebfe8d0241732478ab821.

--- src/main/java/ch/srgssr/playfff/config/AuthenticationConfig.java
+++ src/main/java/ch/srgssr/playfff/config/AuthenticationConfig.java
@@ -50,6 +50,7 @@ public class AuthenticationConfig extends WebSecurityConfigurerAdapter {
         web.ignoring().antMatchers("/api/v{[0-9]+}/deeplink/parsePlayUrl.js").antMatchers(HttpMethod.POST, "/api/v1/deeplink/report");
     }
 
+    @Bean
     @Override
     public UserDetailsService userDetailsService() {
         if (user == null || user.length() == 0 || password == null || password.length() == 0) {
